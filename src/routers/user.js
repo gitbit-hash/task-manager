32bb96fff611e24da850faa9a9fec5f8c39ff145
Muhammad Salah <brokedeveloperhelp@gmail.com>
2022-03-15 23:52:11 +0200

    adding routes to logout user

--- src/routers/user.js
+++ src/routers/user.js
@@ -105,4 +105,28 @@ router.post('/users/login', async (req, res) => {
 	}
 });
 
+router.post('/users/logout', auth, async (req, res) => {
+	try {
+		req.user.tokens = req.user.tokens.filter(
+			(token) => token.token !== req.token
+		);
+		await req.user.save();
+
+		res.send();
+	} catch (error) {
+		res.status(500).send();
+	}
+});
+
+router.post('/users/logoutall', auth, async (req, res) => {
+	try {
+		req.user.tokens = [];
+		await req.user.save();
+
+		res.send();
+	} catch (error) {
+		res.status(500).send();
+	}
+});
+
 module.exports = router;
