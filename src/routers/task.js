5613d7f8e8e0499da0b408c630fcb595b7496f5d
Muhammad Salah <brokedeveloperhelp@gmail.com>
2022-03-16 19:59:42 +0200

    adding the functionality to sord data by createdat property ascending or descending order

--- src/routers/task.js
+++ src/routers/task.js
@@ -22,15 +22,26 @@ router.post('/tasks', auth, async (req, res) => {
 
 router.get('/tasks', auth, async (req, res) => {
 	const match = {};
+	const sort = {};
 
 	if (req.query.completed) {
 		match.completed = req.query.completed === 'true';
+	}
+
+	if (req.query.sortBy) {
+		const parts = req.query.sortBy.split(':');
+		sort[parts[0]] = parts[1] === 'desc' ? -1 : 1;
 	}
 
 	try {
 		await req.user.populate({
 			path: 'tasks',
 			match,
+			options: {
+				limit: parseInt(req.query.limit) || 0,
+				skip: parseInt(req.query.skip) || 0,
+				sort,
+			},
 		});
 		res.send(req.user.tasks);
 	} catch (error) {
