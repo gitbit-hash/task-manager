99e91a35dd897bc80b9c7cd1c1dd6ad99e44fc1a
Muhammad Salah <brokedeveloperhelp@gmail.com>
2022-03-17 21:25:54 +0200

    moving jwt secret and mongoose url to .env file

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -5,6 +5,8 @@ const jwt = require('jsonwebtoken');
 
 const Task = require('./task');
 
+require('dotenv').config();
+
 const userSchema = new Schema(
 	{
 		name: {
@@ -114,7 +116,7 @@ userSchema.methods.toJSON = function () {
 
 userSchema.methods.generateAuthToken = async function () {
 	const user = this;
-	const token = jwt.sign({ _id: user._id.toString() }, 'mySuperSecretJWT');
+	const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET);
 
 	user.tokens = user.tokens.concat({ token });
 
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,10 +1,12 @@
 const jwt = require('jsonwebtoken');
 const User = require('../db/models/user');
 
+require('dotenv').config();
+
 const auth = async (req, res, next) => {
 	try {
 		const token = req.header('Authorization').replace('Bearer ', '');
-		const decoded = jwt.verify(token, 'mySuperSecretJWT');
+		const decoded = jwt.verify(token, process.env.JWT_SECRET);
 		const user = await User.findOne({
 			_id: decoded._id,
 			'tokens.token': token,
